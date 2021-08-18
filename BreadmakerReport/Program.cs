964c1c50d7071a9d0390537a82e0a7b41e4e02ce
Vaidehi <Vdarji@confederationcollege.ca>
2021-08-18 19:53:18 -0400

    final assignment

--- BreadmakerReport/Program.cs
+++ BreadmakerReport/Program.cs
@@ -15,18 +15,28 @@ namespace BreadmakerReport
         {
             Console.WriteLine("Welcome to Bread World");
             var BreadmakerDb = new BreadMakerSqliteContext(dbfile);
+
             var BMList = BreadmakerDb.Breadmakers
-                // TODO: add LINQ logic ...
-                //       ...
+
+               // .Include(i => i.Reviews)
+                .AsEnumerable()
+                .Select(bmd => new {
+                    Reviews = bmd.Reviews.Count,Average = Math.Round(bmd.Reviews.Average(s => s.stars), 2),
+                    Adjust = Math.Round(ratingAdjustmentService.Adjust(bmd.Reviews.Average(s => s.stars), bmd.Reviews.Count()), 2),
+                    bmd.title
+                })
+
+             //   var BMList = BMListT.Select 
+                .OrderByDescending(bmd => bmd.Adjust)
                 .ToList();
 
             Console.WriteLine("[#]  Reviews Average  Adjust    Description");
             for (var j = 0; j < 3; j++)
             {
                 var i = BMList[j];
-                // TODO: add output
-                // Console.WriteLine( ... );
-            }
+              
+               Console.WriteLine("\n[{0}] {1} {2} {3} {4}", j + 1, i.Reviews, i.Average, i.Adjust, i.title); //add output
+            } 
         }
     }
 }
